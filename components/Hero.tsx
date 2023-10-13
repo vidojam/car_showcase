58041fb14ccd327eaedfaa7442c4906d7857a6c4
Jose Torres <vidojam@gmail.com>
2023-10-13 13:50:56 -0400

    Hero component add

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,22 +4,7 @@ import Image from 'next/image';
 
 const Hero = () => {
   return (
-    <div className="hero">
-      <div className="flex-1 pt-36 padding-x">
-        <h1 className="hero__title">
-          Find, book, or rent a car -- quickly and easily.
-        </h1> 
-        <p className="hero__subtitle">
-          Streamline your car rental experience with our effortless booking process.
-        </p>
-
-        <CustomButton
-          title="Explore Cars"
-          containerStyles="bg-primary-blue text-white rounded-full mt-10"
-          handleClick={handleScroll}
-        />
-
-      </div>
-    </div>
+     <div> Hero </div>
   )
 }
+export default Hero;
