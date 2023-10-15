e0aac3c7432bd6a0568b93b1cc6ee9f6b10c824e
Jose Torres <vidojam@gmail.com>
2023-10-15 11:57:43 -0400

    add footer links

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -22,7 +22,14 @@ const Hero = () => {
           text-white rounded-full mt-10"
           handleClick={handleScroll}
           />
+        </div>
+        <div className="hero__image-container">
+          <div className="hero__image">
+            <Image src="/hero.png" alt="hero"
+            fill className="object-contain"/>
 
+            <div className="hero__image-overlay"/>
+          </div>
         </div>
      </div>
   )
