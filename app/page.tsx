00d93eb00d64c76f025ae86f07657b66b1cb3201
Jose Torres <vidojam@gmail.com>
2023-10-20 14:45:33 -0400

    Working on fetchCARS

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
-import Image from 'next/image'
 
-import { Hero } from '@/components'
-import { SearchBar } from '@/components'
-import { CustomFilter } from '@/components'
-import { fetchCars } from '@/utils'
+
+import Image from 'next/image';
+
+import { CustomFilter, Hero, SearchBar } from '@/components';
+import { fetchCars } from '@/utils';
 
 export default async function Home() {
   const allCars = await fetchCars();
 
-  console.log(allCars)
-
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -28,8 +27,19 @@ export default async function Home() {
             <CustomFilter title="year" />
 
           </div>
-
         </div>
+          {!isdataEmpty ? (
+          <section>
+            WE HAVE CARS
+          </section>
+          ) : (
+            <div>
+              <h2>Opps, no results</h2>
+            </div>
+
+          )}
+
+
       </div>
     </main>
   )
