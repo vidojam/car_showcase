7143d98c8a26bd9693b2df17322513884c693a3c
Jose Torres <vidojam@gmail.com>
2023-10-25 17:44:38 -0400

    carRent work

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { useState } from 'react';
+import image from 'next/image';
 
-const CarCard = () => {
+import { CarProps } from '@/types';
+import CustomButton from './CustomButton';
+import { calculateCarRent } from '@/utils';
+
+interface CarCardProps {
+  car: CarProps;
+}
+
+const CarCard = ({car}: CarCardProps) => {
+  const { city_mpg, year, make, model, transmission, drive } = car;
+
+  const carRent = calculateCarRent(city_mpg, year);
+
   return (
-    <div>
-      
+    <div  className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content__title">
+          {make} {model}
+        </h2>
+      </div>
+
+      <p className="flex mt-6 text-[32px] 
+      font-extrabold">
+        <span className="self-start text-[14px] font-semibold">
+          $
+        </span>
+          {carRent}
+        <span className="self-end text-[14px] font-semibold">
+          /day
+        </span>
+      </p>
     </div>
   )
 }
--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,5 @@
 import { MouseEventHandler } from "react";
+import { StringDecoder } from "string_decoder";
 
 export interface CustomButtonProps {
  title: string;
@@ -6,9 +7,26 @@ export interface CustomButtonProps {
  handleClick?:
  MouseEventHandler<HTMLButtonElement>; 
  btnType: "button" | "submit";
+ car: string;
 }
 
 export interface SearchManuFacturerProps {
   manufacturer: string;
   setManuFacturer: (manufacturer: string) => void;
+}
+
+export interface CarProps {
+
+city_mpg:number;
+class:string;
+combination_mpg:number;
+cylinders:number;
+displacement:number;
+drive:string;
+fuel_type:string;
+highway_mpg:number;
+make:string;
+model:string;
+transmission:string;
+year:number;
 }
